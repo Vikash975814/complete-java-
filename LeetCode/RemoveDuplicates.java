f9d9215df45c872e436c4f27e7f8a7a5ff217313
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-04-30 09:59:13 +0530

    Create RemoveDuplicates.java

---
+++ LeetCode/RemoveDuplicates.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int a=1;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]<nums[i+1]){
+                nums[a]=nums[i+1];
+                a++;
+            }
+        }
+        return a;
+    }
+}
