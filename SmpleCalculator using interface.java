c3338e224086a734979858f71bfcbf0c550915e2
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-05-10 01:43:20 +0530

    Create SmpleCalculator using interface.java

---
+++ SmpleCalculator using interface.java
@@ -0,0 +1,30 @@
+
+interface Calculator{
+	int add(int a,int b);
+	int sub(int a,int b);
+	int multiply(int a,int b);
+	int divide(int a,int b);
+}
+class BasicCal implements Calculator{
+	public int add(int a,int b) {
+		return a+b;
+	}
+	public int sub(int a, int b) {
+		return a-b;
+	}
+	public int multiply(int a, int b ) {
+		return a*b;
+	}
+	public int divide(int a, int b) {
+		return a/b;
+	}
+}
+public class vikas{
+	public static void main(String s[]) {
+		BasicCal c=new BasicCal();
+		System.out.println(c.add(10,10));
+		System.out.println(c.sub(10,10));
+		System.out.println(c.multiply(10,10));
+		System.out.println(c.divide(10,10));
+	}
+}
