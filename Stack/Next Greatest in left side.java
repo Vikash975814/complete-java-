db64aecdd430ff928c3f6bead5b251553b211feb
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-05-15 23:50:59 +0530

    Create Next Greatest in left side.java

---
+++ Stack/Next Greatest in left side.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class NxtGreatest {
+	public static void main(String[] args) {
+   Scanner sc=new Scanner(System.in);
+   System.out.print("Enter the size of Array");
+   int n=sc.nextInt();
+   int arr[]=new int[n];
+   System.out.print("Enter the Array elemnt");
+   for(int i=0;i<n;i++) {
+	   arr[i]=sc.nextInt();
+   }
+   Stack<Integer> s=new Stack<>();
+  int nxtgreater[]=new  int[arr.length];
+	for(int i=arr.length-1;i>=0;i--) {
+		while(!s.isEmpty() && arr[s.peek()]>=arr[i]) {
+			s.pop();
+		}
+		if(s.isEmpty()) {
+			nxtgreater[i]=-1;
+		}else {
+			nxtgreater[i]=arr[s.peek()];
+		}
+		s.push(i);
+	}
+	for(int i=0;i<nxtgreater.length;i++) {
+		System.out.print(nxtgreater[i]+" ");
+	}
+	System.out.println();
+	}
+
+}
