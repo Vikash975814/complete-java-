980263ddea88f803bfdc71d17581a06033a1a61a
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-05-23 23:47:49 +0530

    Create isValid.java

---
+++ LeetCode/isValid.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> c=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='[')
+            {
+                c.push(ch);
+}else{
+                if(c.isEmpty()){
+                    return false;
+                }
+                if((c.peek()=='(' && ch==')')||(c.peek()=='{' && ch=='}')
+                  || (c.peek()=='[' && ch==']')){
+                    c.pop();
+}else{
+                    return false;
+                }
+}
+}
+        if(c.isEmpty())
+            return true;
+        else
+            return false;
+    }
+}
