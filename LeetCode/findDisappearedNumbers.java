aa8864cfc316bfb9b57416fdbc4a38bd39242d1d
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-06-16 19:55:14 +0530

    Create findDisappearedNumbers.java

---
+++ LeetCode/findDisappearedNumbers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        ArrayList<Integer> result = new ArrayList<>();
+         for (int i : nums) 
+           set.add(i);
+       for(int i =1; i <=nums.length; i++){
+           if(!set.contains(i)) 
+             result.add(i);
+       }
+        return result;
+    }
+}
