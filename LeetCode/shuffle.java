d174684df947ed01d77d0998685a4574e213b033
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-06-22 01:42:57 +0530

    Create shuffle.java

---
+++ LeetCode/shuffle.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int arr[]=new int[nums.length];
+        int i=0;
+          while(i<n)
+        {
+          arr[2*i]=nums[i];
+          arr[2*i+1]=nums[i+n];
+            i++;
+        }
+        return arr;
+    }
+}
