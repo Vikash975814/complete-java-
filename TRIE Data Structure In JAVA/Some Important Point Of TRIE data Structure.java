8d8b45fdbbd9aece782dd4495a9cc8a7b6e82eec
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-07-10 16:05:37 +0530

    Create Some Important Point Of TRIE data Structure.java

---
+++ TRIE Data Structure In JAVA/Some Important Point Of TRIE data Structure.java
@@ -0,0 +1,6 @@
+TRIE
+1. This is better for AVL or Binary Search Tree.
+2. This Trie Time Complexity is very Optimized that is O(L) where L is the Length of Single Word in Given hole String.
+3.Searching is Very Fast in This Trie.
+4.Root Node is an Empty Node in this Trie.
+5.Prefix is Not Repeated.
