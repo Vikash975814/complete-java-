0793ce26d08e00d065aa9072e3d4d6022af759fe
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-07-12 18:38:28 +0530

    Create findmid.java

---
+++ LeetCode/findmid.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+//     public ListNode findmid(ListNode head){
+//         ListNode slow=head;
+//         ListNode fast=head;
+//         while(fast!=null && fast.next!=null){
+//             slow=slow.next;
+//             fast=fast.next.next;
+// }
+//         return slow;
+// }
+    public int pairSum(ListNode head) {
+    //     ListNode curr=findmid(head);
+    //     ListNode prev=null;
+    //     ListNode next;
+    //     while(curr!=null){
+    //         next=curr.next;
+    //         curr.next=prev;
+    //         prev=curr;
+    //         curr=next;
+    //  }
+    //     ListNode start = head;
+    //     int maximumSum = 0;
+    //     while (prev != null) {
+    //     maximumSum = Math.max(maximumSum, start.val + prev.val);
+    //         prev = prev.next;
+    //         start = start.next;
+    //     }
+
+    //     return maximumSum;
+
+   List<Integer> ll=new ArrayList<>();
+   ListNode temp=head;
+   while(temp!=null)
+   {
+       ll.add(temp.val);
+       temp=temp.next;
+   }
+     int sum,max=0;
+     int i=0,j=ll.size()-1;
+     while(i<j){
+         sum=ll.get(i)+ll.get(j);
+         max=Math.max(max,sum);
+         i++;
+         j--;
+     }
+     return max;
+    }
+}
