9261a7540f35b2ad83dd86131b53275f1e2bdf26
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-07-14 23:34:51 +0530

    Create isBalanced.java

---
+++ LeetCode/isBalanced.java
@@ -0,0 +1,16 @@
+class Solution {
+     public boolean isBalanced(TreeNode root) {
+          return getHeight(root)!=-1;
+     }
+     private int getHeight(TreeNode root) {
+         if (root == null) {
+             return 0;
+         }
+         int lh = getHeight(root. left);
+         int rh = getHeight(root.right);
+         if (lh == -1|| rh==-1||Math. abs(lh - rh) > 1) {
+             return -1;
+         }
+         return Math.max(lh, rh) + 1;
+     }
+}
