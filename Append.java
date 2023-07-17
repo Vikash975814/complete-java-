36b8ccc74fc0818bc79e6607b14a210684dcc561
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-07-17 15:42:08 +0530

    Create Append.java

---
+++ Append.java
@@ -0,0 +1,17 @@
+public String addBinary(String a, String b)  {
+    StringBuilder sb = new StringBuilder();
+    int c = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+    while (i >= 0 || j >= 0 || c == 1) 
+    {
+      if(i >= 0)
+        c =c+ a.charAt(i--) - '0';
+      if(j >= 0)
+        c += b.charAt(j--) - '0';
+      sb.append(c % 2);
+      c = c/2;
+    }
+    return sb.reverse().toString();
+  }
+  }
