36ede2a447e68c192c42c25bbfc1082adfc81cb2
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-08-11 21:55:54 +0530

    Create Rotate array k times.java

---
+++ LeetCode/Rotate array k times.java
@@ -0,0 +1,23 @@
+// 1. reverse all array
+//2.reverse 0 to k-1 ;
+//3.revese k to nums.length;
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k%=nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+    }
+    public void reverse(int [] arr,int i,int j)
+    {
+        while(i<j)
+        {
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
