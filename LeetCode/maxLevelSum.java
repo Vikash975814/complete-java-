0000255b4d5bb13d8eacdfa1c4e0e761c88a4f16
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-10-15 19:37:46 +0530

    Create  maxLevelSum.java

---
+++ LeetCode/maxLevelSum.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int level = 0, ans = 0, sum = Integer.MIN_VALUE;
+        while(!q.isEmpty()){
+            int size = q.size();
+            int max = 0;
+            for(int i = 0; i < size; i++){
+                TreeNode curr = q.poll();
+                max = max + curr.val;
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+                if(curr.right != null){
+                    q.add(curr.right);
+                }
+            }
+            level++;
+            if(max > sum){
+                ans = level;
+                sum = max;
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+
+
