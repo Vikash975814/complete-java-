cadecb047cb6f73d8c0a3122464dfcd2ce2a99a6
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-10-23 01:57:36 +0530

    Create reverseOnlyLetters.java

---
+++ LeetCode/reverseOnlyLetters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        Stack<Character> st=new Stack<>();
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(Character.isLetter(ch))
+            {
+                st.push(ch);
+            }
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(Character.isLetter(ch))
+            {
+                sb.append(st.pop());
+            }else{
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
