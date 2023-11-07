5c077107d15ed7e07c3bfa1b342547b4315edea8
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-11-07 23:35:24 +0530

    Create  sumTriangles.java

---
+++ GFG/sumTriangles.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            
+            int matrix[][] = new int[n][n];
+            for(int i = 0; i < n; i++)
+                for(int j = 0; j < n; j++)
+                    matrix[i][j] = sc.nextInt();
+
+            Solution ob = new Solution();
+            ArrayList<Integer> ans = ob.sumTriangles(matrix,n);
+            for (Integer val: ans) 
+                System.out.print(val+" "); 
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to return sum of upper and lower triangles of a matrix.
+    static ArrayList<Integer> sumTriangles(int matrix[][], int n)
+    {
+        // code here
+        ArrayList<Integer> arr=new ArrayList<>();
+        int upsum=0,lsum=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                lsum+=matrix[i][j];
+            }
+            for(int j=i;j<n;j++)
+            {
+                upsum+=matrix[i][j];
+            }
+        }
+        arr.add(upsum);
+        arr.add(lsum);
+        return arr;
+    }
+}
