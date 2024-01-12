da310d168bb35231dfc98dacdede069c2980da99
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2024-01-13 01:39:29 +0530

    Create  reverseWords.java

---
+++ GFG/reverseWords.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.reverseWords(s));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution 
+{
+    //Function to reverse words in a given string.
+    String reverseWords(String S)
+    {
+        String Words [] = S.split("\\.");
+        StringBuilder reversed = new StringBuilder();
+
+        for(int i = Words.length-1; i >= 0; i--){
+            reversed.append(Words[i]);
+            if(i != 0){
+                reversed.append(".");
+            }
+        }
+        return reversed.toString();
+    }
+}
